9c144959f62790aefa7ba8346f7a81d21602f7ed
teddyplay <kasymovablai8@gmail.com>
2022-09-14 10:28:48 +0600

    django

---
+++ main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+import datetime
+
+
+def about_us(request):
+    return render(request, "about.html")
+
+
+
+def date_now(request):
+   a = datetime.datetime.now()
+   return render(request, 'date_now.html', {'dat': a})
+
