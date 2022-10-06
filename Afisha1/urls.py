f7fbb879910263366f60d5209f25fedf1315fda2
teddyplay <kasymovablai8@gmail.com>
2022-10-06 21:07:55 +0600

    izmenil 4 dz s kartinkami

--- Afisha1/urls.py
+++ Afisha1/urls.py
@@ -1,32 +1,19 @@
-"""Afisha1 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-
-import main
-from main import views
 from django.urls import include
 
 
 
-urlpatterns = [
+urlpatterns = (
+    [
     path('admin/', admin.site.urls),
-    path('about_us/', views.about_us),
-    path('date_now/', views.date_now),
-    path('', include("main.urls")),
+    path('', include('main.urls')),
 
 
-]
+
+] + static(settings.STATIC_URL, document_url=settings.STATIC_URL)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+)
